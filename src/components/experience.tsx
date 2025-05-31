ff6aca2486fb83b7961f3def2d213c51755eada7
Brouks <joshua.waters365@gmail.com>
2025-05-31 13:10:13 -0400

    Added functionality for left vs. right side experiences

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -6,26 +6,31 @@ type ExperienceProps = {
   eName: string, // experience name
   loc?: string,  // location (employer and geospatial)
   points: string[],
-  side?: boolean,
+  side?: boolean, // true means left, false means right
 }
 const Experience: React.FC<ExperienceProps> = ({ iPath, eName, loc, points, side = true }) => {
   const pointList = points.map(point => <li>{point}</li>)
 
   return (
     <>
-      <Stack direction="column">
-        <Box display="inline-flex">
+      <Stack
+        direction="column"
+        marginTop="2% !important"
+        display="flex"
+        sx={{ alignItems: (side ? 'baseline' : 'end')
+      }}>
+        <Box display="inline-flex" width="100%" sx={{ justifyContent: (side ? 'left' : 'right') }}>
         {/* icon */}
-          { !side && (<Avatar src={iPath}/>) }
+          { side && (<Avatar src={iPath}/>) }
         {/* Name and Location */}
-          <Stack direction="column">
+          <Stack direction="column" marginLeft={(side?'1rem':'0rem')} marginRight={(side?'0rem':'1rem')}>
             <Typography>{eName}</Typography>
             <Typography>{loc}</Typography>
           </Stack>
-          { side && (<Avatar src={iPath}/>) }
+          { !side && (<Avatar src={iPath}/>) }
         </Box>
 
-        <Box component="ul">
+        <Box component="ul" textAlign={(side ? 'left' : 'right')} sx={{ direction: (side?'ltr':'rtl') }}>
           {pointList}
         </Box>
       </Stack>
