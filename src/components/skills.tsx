8152dbba3f2be4f5ab2f05cf562c65d251f541ad
Brouks <joshua.waters365@gmail.com>
2025-05-30 20:14:34 -0400

    Added side scrolling on small screens to skills areas and changed sizes

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,4 +1,5 @@
-import { Typography, Stack, Box } from '@mui/material';
+import { Typography, Stack } from '@mui/material';
+import { lighten } from '@mui/system';
 
 type SkillAreaProps = {
   sName: string, // skill name
@@ -7,22 +8,32 @@ type SkillAreaProps = {
 }
 
 const SkillArea: React.FC<SkillAreaProps> = ({ sName, sColor = '#AFAFAF', skills}) => {
+  let num = 0
+
+  const textColor = lighten(sColor, .8)
   const skillList = skills.map(skill =>
-    <Typography sx={{ backgroundColor: sColor }} borderRadius='999px' paddingX='1em' paddingY='.5em'>
+    <Typography sx={{ 
+        backgroundColor: sColor, 
+        color: textColor,
+        flexShrink: 0
+    }} borderRadius='999px' paddingX='.75em' paddingY='.25em' key={++num} fontWeight="bold">
         {skill}
     </Typography>
   );
 
   return (
     <>
-      <Stack direction='column'>
-        <Typography variant='h3'>{sName}</Typography>
+      <Stack direction='column' spacing={.25}>
+        <Typography variant='h6' fontWeight='bold'>{sName}</Typography>
 
         <Stack direction="row"
           spacing={1}
           alignItems="left"
+
           sx={{
             display: 'inline-flex',
+            overflowX: 'auto',
+            whiteSpace: 'nowrap',
           }}>{skillList}</Stack>
       </Stack>
     </>
