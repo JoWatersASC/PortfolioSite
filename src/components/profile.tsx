a1484ea70ddc6120641a2b09be4517e4fb32bab2
Brouks <joshua.waters365@gmail.com>
2025-05-30 20:14:55 -0400

    Various sizing changes to profile

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -17,33 +17,43 @@ const Profile = () => {
        {/* PROFILE PICTURE */}
           <Avatar src={pfp} sx={{
             marginLeft: 'auto',
-            width: '9em',
-            height: '9em'
+            width: '11em',
+            height: '11em'
             }}
           />
           {/* NAME */}
-          <Typography textAlign='right' variant='h4' sx={{ fontWeight: 'bold' }}>
+          <Typography textAlign='right' variant='h3' sx={{ fontWeight: 'bold' }}>
             Joshua Waters
           </Typography>
         </Box>
         {/* TITLE */}
         {/* LOCATION */}
-        <Typography sx={{ fontSize: '.8rem', fontStyle: 'italic !important'}} marginLeft='auto'>
+        <Typography sx={{ fontSize: '1rem', fontStyle: 'italic !important'}} marginLeft='auto'>
           <LOIcon />Atlanta, Georgia, United States
         </Typography>
         {/* RESUME */}
-        <Box display="inline-flex" gap={2} alignItems="center">
-          <Box display="inline-flex" gap={.5}>
-            <RESIcon />
-            <Typography>Resume</Typography>
+        <Box display="inline-flex" gap={1.25} marginLeft="auto" marginRight='0'>
+          <Box display="inline-flex" gap={.5} marginTop="auto" marginBottom="auto">
+            <RESIcon sx={{ fontSize: '2rem' }} />
+            <Typography variant='h6'>Resume</Typography>
           </Box>
-          <ButtonGroup variant='text'>
-            <Button href="/assets/resume.pdf" target="_blank" rel="noopener noreferrer">View</Button>
-            <Button href="/assets/resume.pdf" download>Download</Button>
+          <ButtonGroup variant='outlined' size='large'>
+            <Button 
+              sx={{ fontSize: '1.5rem', fontWeight: 'bold' }} 
+              href="/assets/resume.pdf" target="_blank" rel="noopener noreferrer"
+            >
+              View
+            </Button>
+            <Button 
+              sx={{ fontSize: '1.5rem', fontWeight: 'bold' }} 
+              href="/assets/resume.pdf" download
+            >
+              Download
+            </Button>
           </ButtonGroup>
         </Box>
        {/* SOCIALS */}
-       <Box marginLeft='auto'><Socials /></Box>
+       <Box marginLeft='auto'><Socials space={3}/></Box>
       </Box>
     </>
   )
